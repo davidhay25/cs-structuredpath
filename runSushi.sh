4ec496b1ac1ef1b5040fcae78b8122a5909f27f4
davidhay25 <david.hay25@gmail.com>
2022-06-23 07:12:19 +1200

    builds locally...

--- runSushi.sh
+++ runSushi.sh
@@ -6,12 +6,12 @@ sushi  -s -o .
 if [[ $? -eq 0 ]]
 then
 
-echo "Creating Profiles and extensions summary pages..."
-../scripts/makeProfilesAndExtensions.js cs-structuredpathology
+# echo "Creating Profiles and extensions summary pages..."
+# ../scripts/makeProfilesAndExtensions.js cs-structuredpathology
 echo
 
-echo "Making terminology summary"   # will copy into IG input folder
-../scripts/makeTerminologySummary.js cs-structuredpathology
+#vecho "Making terminology summary"   # will copy into IG input folder
+# ../scripts/makeTerminologySummary.js cs-structuredpathology
 
 echo "Not doing anything else right now"
 
